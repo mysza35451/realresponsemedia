d004337cb965623d15a2699c6ceed21b019a5378
Maciej Myszczynski <maciejm@domain.gtc>
2021-07-09 16:52:14 +0100

    fix of the highlighted wrong inputs and css display of github pages

--- JS/input-validation.js
+++ JS/input-validation.js
@@ -1,6 +1,7 @@
 let requiredInputs = document.querySelectorAll(".required");
 let phone = document.querySelector("#telephone");
 let emailAddress = document.querySelector("#email");
+let postcode = document.querySelector("#postcode");
 
 phone.addEventListener("blur", (event) => {
   let element = document.getElementById(phone.id);
@@ -11,11 +12,18 @@ emailAddress.addEventListener("blur", (event) => {
   email(event.target.value, element);
 });
 
+postcode.addEventListener("blur", (event) => {
+  let element = document.getElementById(postcode.id);
+  postcodeValidation(event.target.value, element);
+});
+
 for (let x = 0; x < requiredInputs.length; x++) {
   console.log(requiredInputs[x].id);
   requiredInputs[x].addEventListener("blur", (event) => {
+    console.log(wrongNoInputCounter);
     let element = document.getElementById(requiredInputs[x].id);
     checkIfNull(event.target.value, element);
+    displayNullInputWarning();
   });
 }
 //warning messages
@@ -23,19 +31,16 @@ let wrongNoInputCounter = 0;
 let warningMessageNoInput = document.getElementById("empty-input");
 let warningMessageWrongPhone = document.getElementById("wrong-phone-input");
 let warningMessageWrongEmail = document.getElementById("wrong-email-input");
+let warningMessageWrongPostCode = document.getElementById(
+  "wrong-postcode-input"
+);
 
 function checkIfNull(value, element) {
   if (value == "") {
-    element.classList.add("wrong-input");
-    warningMessageNoInput.classList.remove("hidden");
-    wrongNoInputCounter++;
+    element.classList.add("wrong-input-null");
   } else {
     try {
-      wrongNoInputCounter--;
-      element.classList.remove("wrong-input");
-      if (wrongNoInputCounter == 0) {
-        warningMessageNoInput.classList.add("hidden");
-      }
+      element.classList.remove("wrong-input-null");
     } catch (error) {}
   }
 }
@@ -71,5 +76,28 @@ function email(value, element) {
   } else {
     element.classList.remove("wrong-input");
     warningMessageWrongEmail.classList.add("hidden");
+  }
+}
+
+function postcodeValidation(value, element) {
+  let regex =
+    /([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/;
+
+  if (!value.match(regex)) {
+    element.classList.add("wrong-input");
+    warningMessageWrongPostCode.classList.remove("hidden");
+  } else {
+    element.classList.remove("wrong-input");
+    warningMessageWrongPostCode.classList.add("hidden");
+  }
+}
+
+function displayNullInputWarning() {
+  let warningNull = document.getElementsByClassName("wrong-input-null");
+  console.log(warningNull.length);
+  if (warningNull.length == 0) {
+    warningMessageNoInput.classList.add("hidden");
+  } else {
+    warningMessageNoInput.classList.remove("hidden");
   }
 }
